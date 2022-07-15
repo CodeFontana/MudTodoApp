d6e4c43353167cf96f5cb58f628f104797b5003b
Brian Fontana <brian@codefoxtrot.com>
2022-07-15 00:25:39 -0400

    Theme updates
    Also save the theme to browser local storage

--- MudTodo.Server/Program.cs
+++ MudTodo.Server/Program.cs
@@ -1,13 +1,14 @@
+using Blazored.LocalStorage;
 using Microsoft.EntityFrameworkCore;
 using MudBlazor.Services;
 using MudTodo.Shared;
 
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddMudServices();
+builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddDbContext<TodoDbContext>(options =>
 {
     options.UseSqlite($@"Data Source={Environment.CurrentDirectory}\Todo.db;");
@@ -20,22 +21,15 @@ using IServiceScope scope = app.Services.CreateScope();
 TodoDbContext db = scope.ServiceProvider.GetRequiredService<TodoDbContext>();
 db.Database.Migrate();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() == false)
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-
 app.UseRouting();
-
-
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
-
 app.Run();
